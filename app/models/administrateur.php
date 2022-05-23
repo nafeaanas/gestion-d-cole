074625b532a72debd4034bbd3fe8c2d3e965b4ce
kerkazou <kerkazou.zakaria@gmail.com>
2022-05-23 17:22:01 +0100

    fin du brief

--- app/models/administrateur.php
+++ app/models/administrateur.php
@@ -14,12 +14,27 @@ class Administrateur {
         return $result;
     }
 
-    // Statistique des étudiants
+    // Statistique des administrateur
     public function nombreAdministrateur(){
         $this->db->query('SELECT * FROM `administrateur`');
         $result = $this->db->resultSet();
         $result = $this->db->rowCount();
         return $result;
     }
-    
+
+    // Ajoure administrateur
+    public function register($data){
+        $this->db->query('INSERT INTO administrateur (nom, prenom, role, password, email) VALUES (:nom, :prenom, :role, :password, :email)');
+        $this->db->bind(':nom', $data['nom']);
+        $this->db->bind(':prenom', $data['prenom']);
+        $this->db->bind(':role', $data['role']);
+        $this->db->bind(':password', $data['password']);
+        $this->db->bind(':email', $data['email']);
+
+        if($this->db->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
